906951c50e019b3d0680090eb1d0709d3a08f7e5
Kuscko <patrick.kellyse@gmail.com>
2021-06-09 16:21:42 -0400

    Day 3 - pt1 - Completed

--- day3/day3-pt1.py
+++ day3/day3-pt1.py
@@ -1,3 +1,6 @@
+# Advent of Code: Day 3 - Part 1
+# completed by Patrick Kelly 6/9/2021 | 4:21PM
+
 import os
 
 def create_list(file_name):
@@ -17,7 +20,8 @@ x_cordinate = 0
 tree_count = 0
 
 # loop through each line and if x cordinate in line is "#", increment tree counter
-# each value 
+# each line has a length of 32 and to prevent a IndexError exception for the x cordinate
+#                the x cordinate has to be divided by the result of % length of the line. 
 for line in input_data:
     if line[x_cordinate] == "#":
         tree_count += 1
