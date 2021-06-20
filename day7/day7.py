fcaffc90f31b70fe2f33ba1d38c33b269076e5e9
Kuscko <patrick.kellyse@gmail.com>
2021-06-19 20:47:37 -0400

    Day 7 Updated

--- day7/day7.py
+++ day7/day7.py
@@ -1,23 +1,32 @@
 # Advent of Code 2020 Day 7 - Handy Haversacks
 
 '''
-    6/13/2021
-    Updates required to comments, due tomorrow morning around 11am.
+    Back burner Part 2.
 '''
 
+from collections import defaultdict
 import re
 
 class Solution:
     def __init__(self, rules):
         self.rules = rules
 
-    def find_goal(self, goal):
-        for bag in bag_rules:
-            bags = bag_rules[bag]
-            if goal in bags:
-                Solution.find_goal(self, bag)
-                bag_list_count.add(bag)
-        return
+    def get_solution_pt1(self, goal):
+        '''
+            Go through each bag dictionary item and determine which dictionary item has shiny gold bags.
+            Then add them to the counter and get the legnth.
+            Counter Set is left out side of the definition, although recursion could be used by creating
+            a function() inside the method.
+
+            Args:
+                Self -> Dictionary{} rules
+                goal -> "Shiny Gold" or goal of part 1.
+        '''
+        for bags in self:
+            if goal in self[bags]:
+                count.add(bags)
+                Solution.get_solution_pt1(self, bags)
+        return 
 
 # Dictionary class
 class Dictionary:
@@ -28,22 +37,21 @@ class Dictionary:
         '''
             Function that creates a dictionary from the passport list under the item arguement
         '''
-        dictionary = {}
+        dictionary = defaultdict(dict)
         for line in self:
-            bags = re.match("(.*) bag contains", line).group()[0]
-            for bag in bags:
+            top_bag = re.match(r'(.*) bags contain', line).group(1)
+            for value, key in re.findall(r'(\d+) (\w+ \w+) bag', line):
+                dictionary[top_bag][key] = int(value)
         return dictionary
 
-# Create data set of "bag_rules"
+# Create data set of "bag_list"
 with open('inputs/day_07-input.txt', 'r' ) as input: 
-    raw_input1 = input.read().splitlines() # divide each line into a list
-    raw_input2 = [raw_input2.replace(' bags', '').replace('bags,', '').replace('bag,', '').replace('.', '') for raw_input2 in raw_input1] # remove uneccessary information from input strings
-    bag_rules = Dictionary.create_dictionary(raw_input2) # create the dictionary
-    input.close() # close the input file.
-    
-    # initialize set() to use as a filter to remove repeatable bag types
-    bag_list_count = set()
-    Solution.find_goal(bag_rules, "shiny gold")
-    print("Solution 1: ", len(bag_list_count))
+    bag_rules = input.read().splitlines() # divide each line into a list
+    bag_list = Dictionary.create_dictionary(bag_rules) # create the dictionary
 
+    # initialize set() to use as a filter to remove repeatable bag types
+    count = set()
+    Solution.get_solution_pt1(bag_list, "shiny gold")
+    print(len(count))
+    
 
